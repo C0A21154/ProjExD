f87dd83cdcec1a744b62340bf0603ec715239ec0
Nishiyama <c0a21154ca@edu.teu.ac.jp>
2022-06-21 18:11:21 +0900

    修正しました #5

--- ex02/calc.py
+++ ex02/calc.py
@@ -21,14 +21,14 @@ def button_click(event):
     elif (txt == "%"):
         ans = (eval(entry.get())) * 100
         entry.delete(0, tk.END)
-        entry.insert(tk.END, f"{ans}")
+        entry.insert(tk.END, ans)
 
-    entry.insert(tk.END, f"{txt}")
+    entry.insert(tk.END, txt)
 
 def enter():
     ans = eval(entry.get())
     entry.delete(0, tk.END)
-    entry.insert(tk.END, f"{ans}")
+    entry.insert(tk.END, ans)
 
 dentaku = tk.Tk()
 dentaku.title("電卓")
@@ -39,37 +39,17 @@ option = [" ","%", "C", "AC"]
 entry = tk.Entry(dentaku, width = 15, justify = 'right', font = ("Times New Roman", 35))
 entry.grid(columnspan = 10)
 
-for i in range(4):
-    a = tk.Button(text = f"{option[i]}", width = 4, height = 1, font = ("Times New Roman", 30))
-    a.bind("<1>", button_click)
-    a.grid(row = 1, column = i)
-
-
-for i in range(4):
-    a = tk.Button(text = f"{enzansi[i]}", width = 4, height = 1, font = ("Times New Roman", 30))
-    a.bind("<1>", button_click)
-    a.grid(row = i + 2, column = 3)
-
-for i in range(9, -3,-1):
-    if (i == 0):
-        a = tk.Button(text = f"{i}", width = 4, height = 1, font = ("Times New Roman", 30))
-        a.bind("<1>", button_click)
-        a.grid(row = 5, column = 1)
-    
-    elif (i == -1):
-        a = tk.Button(text = "=", width = 4, height = 1, font = ("Times New Roman", 30))
-        a.bind("<1>", button_click)
-        a.grid(row = 5, column = 2)
-
-    elif (i == -2):
-        a = tk.Button(text = ".", width = 4, height = 1, font = ("Times New Roman", 30))
-        a.bind("<1>", button_click)
-        a.grid(row = 5, column = 0)
-        
-    else :
-        a = tk.Button(text = f"{i}", width = 4, height = 1, font = ("Times New Roman", 30)) 
-        a.bind("<1>", button_click)
-        a.grid(row = 2 + (9 - i) // 3, column = (9 - i) % 3)
+for c,i in enumerate(["","%","C","AC",7,8,9,"÷",4,5,6,"×",1,2,3,"-",".",0,"=","+"]):
+        #ボタンの設定
+        button=tk.Button(dentaku,
+                      text=f"{i}",
+                      width=4,
+                      height=1,
+                      font=("Times New Roman",30))
+        button.bind("<1>",button_click)
+        x=c%4+1
+        y=c//4+1
+        button.grid(row=y,column=x)
    
 
 dentaku.mainloop()
