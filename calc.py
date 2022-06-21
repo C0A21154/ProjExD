584ae1eea4b4e6542cd2fb3b5a1290004dfb5526
Nishiyama <c0a21154ca@edu.teu.ac.jp>
2022-06-21 16:48:30 +0900

    四則演算追加

--- calc.py
+++ calc.py
@@ -8,32 +8,58 @@ def button_click(event):
     if (txt == "="):
         enter()
         return 0
+    elif (txt == "×"):
+        txt = "*"
+    elif (txt == "÷"):
+        txt = "/"
+
     entry.insert(tk.END, f"{txt}")
 
 def enter():
     ans = eval(entry.get())
-    print(ans)
     entry.delete(0, tk.END)
     entry.insert(tk.END, f"{ans}")
 
 dentaku = tk.Tk()
 dentaku.title("電卓")
-dentaku.geometry("300x600")
+dentaku.geometry("400x500")
+enzansi = ["÷", "×", "-", "+"]
+option = [" ","%", "c", "ac"]
 
-entry = tk.Entry(dentaku, width = 10, justify = 'right', font = ("Times New Roman", 40))
+entry = tk.Entry(dentaku, width = 15, justify = 'right', font = ("Times New Roman", 40))
 entry.grid(columnspan = 10)
 
+for i in range(4):
+    a = tk.Button(text = f"{option[i]}", width = 4, height = 1, font = ("Times New Roman", 30))
+    a.bind("<1>", button_click)
+    a.grid(row = 1, column = i)
+
+
+for i in range(4):
+    a = tk.Button(text = f"{enzansi[i]}", width = 4, height = 1, font = ("Times New Roman", 30))
+    a.bind("<1>", button_click)
+    a.grid(row = i + 2, column = 3)
+
 for i in range(9, -3,-1):
-    if (i == -1):
-        a = tk.Button(text = "+", width = 4, height = 2, font = ("Times New Roman", 30))
+    if (i == 0):
+        a = tk.Button(text = f"{i}", width = 4, height = 1, font = ("Times New Roman", 30))
+        a.bind("<1>", button_click)
+        a.grid(row = 5, column = 1)
     
+    elif (i == -1):
+        a = tk.Button(text = "=", width = 4, height = 1, font = ("Times New Roman", 30))
+        a.bind("<1>", button_click)
+        a.grid(row = 5, column = 2)
+
     elif (i == -2):
-        a = tk.Button(text = "=", width = 4, height = 2, font = ("Times New Roman", 30), command = enter)
+        a = tk.Button(text = ".", width = 4, height = 1, font = ("Times New Roman", 30))
+        a.bind("<1>", button_click)
+        a.grid(row = 5, column = 0)
         
     else :
-        a = tk.Button(text = f"{i}", width = 4, height = 2, font = ("Times New Roman", 30)) 
-
-    a.bind("<1>", button_click)
-    a.grid(row = 1 + (9 - i) // 3, column = (9 - i) % 3)
+        a = tk.Button(text = f"{i}", width = 4, height = 1, font = ("Times New Roman", 30)) 
+        a.bind("<1>", button_click)
+        a.grid(row = 2 + (9 - i) // 3, column = (9 - i) % 3)
+   
 
 dentaku.mainloop()
