8a44a9e733a5af3108bcce9f74abf78b2d1e8f66
Nishiyama <c0a21154ca@edu.teu.ac.jp>
2022-07-05 17:09:39 +0900

    ４限追加機能実装

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -2,9 +2,16 @@ import random
 from turtle import right
 import pygame as pg
 import sys
+import tkinter as tk
+import tkinter.messagebox as tkm
+import datetime
 
 def main():
-    vx, vy = 1, 1
+    st = datetime.datetime.now()
+    vx1, vy1 = 1, 1
+    vx2, vy2 = -1, 1
+    time = 0
+    bomb_list = []
     
     pg.display.set_caption("逃げろ！！こうかとん")
     
@@ -25,7 +32,16 @@ def main():
     bmimg_sfc.set_colorkey(0)
     bmimg_rct = bmimg_sfc.get_rect()
     bmimg_rct.centerx = random.randint(0, screen_rct.width)
-    bmimg_rct.centery = random.randint(0, screen_rct.height)
+    bmimg_rct.centery = 9
+    bomb_list.append(bmimg_rct)
+    
+    bmimg2_sfc = pg.Surface((20, 20))
+    pg.draw.circle(bmimg2_sfc, (255, 0, 0), (10, 10), 10)
+    bmimg2_sfc.set_colorkey(0)
+    bmimg2_rct = bmimg_sfc.get_rect()
+    bmimg2_rct.centerx = random.randint(0, screen_rct.width)
+    bmimg2_rct.centery = 9
+    bomb_list.append(bmimg2_rct)
 
     while True:
         screen_sfc.blit(bgimg_sfc, bgimg_rct)
@@ -55,13 +71,35 @@ def main():
 
         screen_sfc.blit(bdimg_sfc, bdimg_rct)
 
-        bmimg_rct.move_ip(vx, vy)
+        bmimg_rct.move_ip(vx1, vy1)
+        bmimg2_rct.move_ip(vx2, vy2)
         screen_sfc.blit(bmimg_sfc, bmimg_rct)
-        yoko, tate = bound(bmimg_rct, screen_rct)
-        vx *= yoko
-        vy *= tate 
+        screen_sfc.blit(bmimg2_sfc, bmimg2_rct)
+        yoko1, tate1 = bound(bmimg_rct, screen_rct)
+        yoko2, tate2 = bound(bmimg2_rct, screen_rct)
+        vx1 *= yoko1
+        vy1 *= tate1
+        vx2 *= yoko2
+        vy2 *= tate2 
 
-        if bdimg_rct.colliderect(bmimg_rct) : return
+        if bdimg_rct.colliderect(bmimg_rct) :
+            fi = datetime.datetime.now()
+            tkm.showinfo("GameOver", f"残念また遊んでね！\n生存時間{(fi - st).seconds}秒")
+            return
+        if bdimg_rct.colliderect(bmimg2_rct) :
+            fi = datetime.datetime.now()
+            tkm.showinfo("GameOver", f"残念また遊んでね！\n生存時間{(fi - st).seconds}秒")
+            return
+
+        time += 1
+
+        if time % 1500 == 0:
+            if vx1 < 5.0:
+                vx1 *= 1.2
+                vy1 *= 1.2
+                vx2 *= 1.2
+                vy2 *= 1.2
+
         
         pg.display.update()
         clock.tick(1000)
@@ -75,6 +113,8 @@ def bound(rct, scr_rct):
     return yoko, tate
 
 if __name__ == "__main__":
+    root = tk.Tk()
+    root.withdraw()
     pg.init()
     main()
     pg.quit()
