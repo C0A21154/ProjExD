781c50da13c078c6611adfdad5aaebb74fbe49d0
Nishiyama <c0a21154ca@edu.teu.ac.jp>
2022-07-19 17:59:32 +0900

    カード表示

--- ex06/blackjeck.py
+++ ex06/blackjeck.py
@@ -64,42 +64,157 @@ def Judge(m, d, mw, dw, d_hand):
         dw += 1
 
 
+class Screen:
+    def __init__(self, title, wh, image):
+        pg.display.set_caption(title)
+        self.sfc = pg.display.set_mode(wh) 
+        self.rct = self.sfc.get_rect()  
+        self.bgi_sfc = pg.image.load(image)   
+        self.bgi_rct = self.bgi_sfc.get_rect()
+        self.sfc.blit(self.bgi_sfc, self.bgi_rct)
+
+    def blit(self):
+        self.sfc.blit(self.bgi_sfc, self.bgi_rct)
+
+
+class Card:
+    def __init__(self, image, size, xy):
+        self.sfc = pg.image.load(image)  
+        self.sfc = pg.transform.rotozoom(self.sfc, 0, size)  
+        self.rct = self.sfc.get_rect()  
+        self.rct.center = xy
+
+
+    def blit(self, scr: Screen):
+        scr.sfc.blit(self.sfc, self.rct)
+
+
+class Hit_s:
+    def __init__(self, image, size, xy):
+        self.sfc = pg.image.load(image)  
+        self.sfc = pg.transform.rotozoom(self.sfc, 0, size)  
+        self.rct = self.sfc.get_rect()  
+        self.rct.center = xy
+
+
+    def blit(self, scr: Screen):
+        scr.sfc.blit(self.sfc, self.rct)
+
+
+class Stand_s:
+    def __init__(self, image, size, xy):
+        self.sfc = pg.image.load(image)  
+        self.sfc = pg.transform.rotozoom(self.sfc, 0, size)  
+        self.rct = self.sfc.get_rect()  
+        self.rct.center = xy
+
+
+    def blit(self, scr: Screen):
+        scr.sfc.blit(self.sfc, self.rct)
+
+
+class Back:
+    def __init__(self, image, size, xy):
+        self.sfc = pg.image.load(image)  
+        self.sfc = pg.transform.rotozoom(self.sfc, 0, size)  
+        self.rct = self.sfc.get_rect()  
+        self.rct.center = xy
+
+
+    def blit(self, scr: Screen):
+        scr.sfc.blit(self.sfc, self.rct)
+
 def main():
     mwin = 0
     dwin = 0
 
-    pg.display.set_caption("ブラックジャック")
-    screen_sfc=pg.display.set_mode((1600,900))
-    screen_rct=screen_sfc.get_rect()
-    bgimg_sfc=pg.image.load("fig/bg.jpg")
-    bgimg_rct=bgimg_sfc.get_rect()
-
-    hitimg_sfc=pg.image.load("fig/hit.png")
-    hitimg_sfc=pg.transform.rotozoom(hitimg_sfc, 0, 0.3)
-    hitimg_rct=hitimg_sfc.get_rect()
-    hitimg_rct.center=1200,600
-
-    stdimg_sfc=pg.image.load("fig/stand.png")
-    stdimg_sfc=pg.transform.rotozoom(stdimg_sfc, 0, 0.3)
-    stdimg_rct=stdimg_sfc.get_rect()
-    stdimg_rct.center=1400,600
-
+    scr = Screen("ブラックジャック", (1600, 900), "fig/bg.jpg")
+    hit_s = Hit_s("fig/hit.png", 0.3, (1200,600))
+    std = Stand_s("fig/stand.png", 0.3, (1400,600))
+    bak = Back("fig/back.png", 0.8, (900,200))
     
     while True:
         m_hand = Hand()
         d_hand = Hand()
         m_num = Return_sum(m_hand)
         d_num = Return_sum(d_hand)
-        screen_sfc.blit(bgimg_sfc,bgimg_rct)
-        screen_sfc.blit(hitimg_sfc,hitimg_rct)
-        screen_sfc.blit(stdimg_sfc,stdimg_rct)
+        ct = 1
+        m_card = []
+        d_card = []
+
+        for i in range(4):
+            for j in range(13):
+                if (m_hand[i][j] == True) and ct == 1:
+                    a = i
+                    if i == 1:
+                        a = 13
+                    elif i == 2:
+                        a = 26
+                    elif i == 3:
+                        a = 39
+                    n = a + j
+                    m_card.append(n)
+                    ct += 1
+                if (m_hand[i][j] == True) and ct == 2:
+                    a = i
+                    if i == 1:
+                        a = 13
+                    elif i == 2:
+                        a = 26
+                    elif i == 3:
+                        a = 39
+                    n = a + j
+                    m_card.append(n)
+                    break
+        mc1 = Card(f"fig/{m_card[0]}.png", 0.3, (600, 500))
+        mc2 = Card(f"fig/{m_card[1]}.png", 0.3, (900, 500))
+
+        for i in range(4):
+            for j in range(13):
+                if (d_hand[i][j] == True) and ct == 1:
+                    a = i
+                    if i == 1:
+                        a = 13
+                    elif i == 2:
+                        a = 26
+                    elif i == 3:
+                        a = 39
+                    n = a + j
+                    d_card.append(n)
+                    ct += 1
+                if (m_hand[i][j] == True) and ct == 2:
+                    a = i
+                    if i == 1:
+                        a = 13
+                    elif i == 2:
+                        a = 26
+                    elif i == 3:
+                        a = 39
+                    n = a + j
+                    d_card.append(n)
+                    break
+        
+        dc1 = Card(f"fig/{d_card[0]}.png", 0.3, (600, 200))
+        dc2 = Card(f"fig/{d_card[1]}.png", 0.3, (900, 200))
+
+        scr.blit()
+        hit_s.blit(scr)
+        std.blit(scr)
+        dc1.blit(scr)
+        dc2.blit(scr)
+        bak.blit(scr)
+        mc1.blit(scr)
+        mc2.blit(scr)
 
         for event in pg.event.get():
+            if event.type == pg.QUIT: 
+                return
+
             if event.type == pg.MOUSEBUTTONDOWN:
-                if hitimg_rct.collidepoint(event.pos):
+                if hit_s.rct.collidepoint(event.pos):
                     m_hand = Hit(m_hand)
                     m_num = Return_sum(m_hand)
-                elif stdimg_rct.collidepoint(event.pos):
+                elif std.rct.collidepoint(event.pos):
                     Judge(m_num, d_num, mwin, dwin, d_hand)
 
         if (mwin == 5) or (dwin == 5):
